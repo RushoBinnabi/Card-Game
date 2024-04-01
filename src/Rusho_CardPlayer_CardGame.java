b95f468b017bd4ae338f3575f28b516e1f56a869
RushoBinnabi <69098829+RushoBinnabi@users.noreply.github.com>
2024-04-01 06:37:39 -0400

    Update Rusho_CardPlayer_CardGame.java

--- src/Rusho_CardPlayer_CardGame.java
+++ src/Rusho_CardPlayer_CardGame.java
@@ -12,14 +12,14 @@ public class Rusho_CardPlayer_CardGame {
      * this class acts as the hands for the card game.
      */
 
-    ArrayList<Rusho_Card_CardGame> cards; // creates an arraylist of the card type from the class that acts as the cards for the card game called cards.
+    ArrayList<Rusho_Card_CardGame> cards;
 
     /**
      * the constructor initializes the arraylist as a new arraylist.
      */
 
     public Rusho_CardPlayer_CardGame() {
-        cards = new ArrayList<>(); // initializes the arraylist as a new arraylist.
+        cards = new ArrayList<>();
     }
 
     /**
@@ -27,7 +27,7 @@ public class Rusho_CardPlayer_CardGame {
      */
 
     public void clear_cards() {
-        cards.clear(); // clears the cards.
+        cards.clear();
     }
 
     /**
@@ -36,7 +36,7 @@ public class Rusho_CardPlayer_CardGame {
      */
 
     public void add_card(Rusho_Card_CardGame card) {
-        cards.add(card); // adds the card to cards.
+        cards.add(card);
     }
 
     /**
@@ -45,28 +45,28 @@ public class Rusho_CardPlayer_CardGame {
      */
 
     public String showCardPlayerHand() {
-        String str = ""; // creates a string variable called str that gets initialized to an empty string.
-        for (Rusho_Card_CardGame c: cards) { // this enhanced for loop says that for each card in the number of cards that is tied with the variable c.
-            str += c.toString() + "\n"; // the value of the toString method from the card gets added to str along with a new line.
+        String str = "";
+        for (Rusho_Card_CardGame c: cards) { 
+            str += c.toString() + "\n";
         }
-        return str; // returns str.
+        return str;
     }
 
     /**
      * this method gives some cards to the hands.
      * @param card this arguement says which hand to give the cards to.
-     * @param secondhand // this arguement says how many cards to give to the hands.
+     * @param secondhand this arguement says how many cards to give to the hands.
      * @return returns the boolean value of either true or false depending on whether if the number of cards that were given has the card that was given or not.
      */
 
     public boolean give_card(Rusho_Card_CardGame card, Rusho_CardPlayer_CardGame secondhand) {
-        if (!cards.contains(card)) { // if the number of cards doesn't have the card that was given from the arguement.
-            return false; // then it returns a false.
+        if (!cards.contains(card)) {
+            return false;
         }
-        else { // if the number of cards does have the card that was given from the arguement.
-            cards.remove(card); // the given card is removed from the number of cards.
-            secondhand.add_card(card); // the card that was removed gets added to the "second hand".
-            return true; // then it returns a true.
+        else {
+            cards.remove(card);
+            secondhand.add_card(card);
+            return true;
         }
     }
 }
